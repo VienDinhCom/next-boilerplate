6291aa435afe1407527808edbe338d1d3531210c
Vien Dinh <maxvien@gmail.com>
2019-03-07 16:31:24 +0700

    Add flow support to custom server

--- app/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Home() {
-  return <div>Welcome to next.js!</div>
-}
-
-export default Home
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Index() {
+  return <div>Welcome to next.js!</div>;
+}
+
+export default Index;
